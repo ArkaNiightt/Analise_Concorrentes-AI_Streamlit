b8a4dd7aef83b64e2ae391e52eed7518d4656e9a
ArkaNiightt <augustojoao60@yahoo.com>
2024-12-04 10:49:03 -0300

    initial commit and changes connection db

---
+++ dashbords/analise_with_gpt.py
@@ -0,0 +1,110 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import psycopg2
+import os
+import pandas as pd
+
+load_dotenv()
+
+db_config = {
+    "dbname": os.getenv("POSTGRES_DB"),
+    "user": os.getenv("POSTGRES_USER"),
+    "password": os.getenv("POSTGRES_PASSWORD"),
+    "host": os.getenv("POSTGRES_HOST"),
+    "port": os.getenv("POSTGRES_PORT"),
+}
+
+
+def get_data_from_postgres(ownerusername):
+    try:
+        conn = psycopg2.connect(**db_config)
+        cur = conn.cursor()
+
+        query = f"""
+            SELECT
+            ownerusername,
+            type,
+            likescount,
+            commentscount,
+            videoviewcount,
+            videoplaycount,
+            hashtags,
+            mentions,
+            caption
+            FROM
+            dataset_items
+            WHERE
+            ownerusername = %s;
+        """
+        cur.execute(query, (ownerusername,))
+
+        columns = [desc[0] for desc in cur.description]
+        data = cur.fetchall()
+
+        cur.close()
+        conn.close()
+
+        return columns, data
+
+    except Exception as e:
+        print(f"Erro ao conectar ao banco de dados: {e}")
+        return [], []
+
+
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+
+def analyze_data_with_gpt(columns, data, model="gpt-4o", temperature=0.5):
+    if not columns or not data:
+        return "Não foi possível obter os dados para análise."
+
+    df = pd.DataFrame(data, columns=columns)
+    data_str = df.to_string(index=False)
+
+    prompt = f"""Analise os seguintes dados do Instagram e forneça insights de marketing:
+
+    Colunas: {', '.join(columns)}
+
+    Dados:
+    {data_str}
+
+    Com base nesses dados, forneça uma análise comparativa dos resultados das empresas, observando o engajamento e oferecendo recomendações de marketing:
+    1. Comparação entre as diferentes empresas em termos de engajamento (comentários e curtidas).
+    2. Tipos de conteúdo que têm melhor desempenho para cada empresa.
+    3. Padrões de engajamento que se destacam.
+    4. Melhores horários para postagem para maximizar o engajamento.
+    5. Usuários mais influentes em cada empresa.
+    6. Desempenho de vídeos em comparação a outros tipos de conteúdo.
+    7. Temas ou tópicos que geram mais engajamento.
+    8. Recomendações específicas para melhorar o engajamento de cada empresa.
+    9. Segmente semanticamente pelo significado das palavras, categorizando cada parte dos elementos 'captions' enviada em {data_str}, com isso crie categorias [
+    "Recursos e ferramentas",
+    "Motivacional",
+    "Eventos e Promoções",
+    "Histórias de sucesso",
+    "Educacional",
+    "Dicas e conselhos",
+    "Humor/Descontração", Crie mais 10 categorias...] que mais aparecem, mostrando quantas vezes aparece.
+
+    Por favor, forneça uma análise detalhada e acionável para cada ponto. Vale entender que alguns valores vão estar com valor -1, isso quer dizer que foi desabilitada, por exemplo: 'likescount=-1 quer dizer que os comentários foram desabilitados.'
+    """
+
+    try:
+
+        response = client.chat.completions.create(
+            model=model,
+            temperature=temperature,
+            messages=[
+                {
+                    "role": "system",
+                    "content": "Você é um analista de marketing especializado em dados do Instagram.",
+                },
+                {"role": "user", "content": prompt},
+            ],
+        )
+
+        return response.choices[0].message.content
+
+    except Exception as e:
+        print(f"Erro ao chamar a API do OpenAI: {e}")
+        return "Erro ao obter resposta do GPT para este conjunto de dados."
---
+++ dashbords/dashboard_streamlit_postgres.py
@@ -0,0 +1,262 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+from dotenv import load_dotenv
+from analise_with_gpt import analyze_data_with_gpt
+import logging
+from io import BytesIO
+from supabase import create_client, Client
+
+# Configuração da página deve ser a primeira chamada
+st.set_page_config(page_title="Análise de Dados do Usuário", page_icon="📈")
+
+# Configuração do logging
+logging.basicConfig(
+    level=logging.ERROR, format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+# Carregar variáveis de ambiente
+load_dotenv()
+
+# Configuração do Supabase
+supabase_url = st.secrets["SUPABASE_URL"]
+supabase_key = st.secrets["SUPABASE_KEY"]
+supabase: Client = create_client(supabase_url, supabase_key)
+
+
+class DatabaseConnection:
+    @staticmethod
+    @st.cache_data
+    def get_data():
+        try:
+            response = supabase.table("dataset_items").select("*").execute()
+            df = pd.DataFrame(response.data)
+            return df
+        except Exception as e:
+            logging.error(f"Erro ao conectar ao banco de dados: {e}")
+            st.error(f"Erro ao conectar ao banco de dados: {e}")
+            return pd.DataFrame()
+
+
+class Dashboard:
+    def __init__(self):
+        self.data = DatabaseConnection.get_data()
+        self.data_filtered = None
+        self.selected_username = None
+
+    def render_sidebar(self):
+        st.sidebar.header("Filtros")
+        if not self.data.empty:
+            self.selected_username = st.sidebar.selectbox(
+                "Selecione o dono do post: ",
+                options=self.data["ownerusername"].unique(),
+            )
+            self.data_filtered = self.data[
+                self.data["ownerusername"] == self.selected_username
+            ]
+            if st.sidebar.button("Atualizar Dados"):
+                st.cache_data.clear()
+                self.data = DatabaseConnection.get_data()
+        else:
+            st.sidebar.write("Nenhum dado disponível para filtrar.")
+
+    def render_overview(self):
+        if self.selected_username:
+            st.title(
+                f"📈 Análise Interativa dos Dados do Usuário: {self.selected_username}"
+            )
+            st.markdown(
+                "Explore as métricas e dados de postagens, visualizando insights importantes de forma interativa e atraente."
+            )
+
+    def render_post_type_distribution(self):
+        if self.data_filtered is not None and not self.data_filtered.empty:
+            post_type_counts = self.data_filtered["type"].value_counts()
+            fig = px.pie(
+                names=post_type_counts.index,
+                values=post_type_counts,
+                title="Distribuição dos Tipos de Postagens",
+                hole=0.4,
+                color_discrete_sequence=px.colors.sequential.RdBu,
+            )
+            st.plotly_chart(fig)
+
+    def render_general_statistics(self):
+        if self.data_filtered is not None and not self.data_filtered.empty:
+            st.subheader("📈 Estatísticas Gerais")
+            if (
+                "commentscount" in self.data_filtered.columns
+                and "likescount" in self.data_filtered.columns
+            ):
+                avg_comments = self.data_filtered["commentscount"].clip(lower=0).mean()
+                avg_likes = self.data_filtered["likescount"].clip(lower=0).mean()
+                col1, col2 = st.columns(2)
+                with col1:
+                    st.metric(
+                        label="Média de Comentários",
+                        value=f"{avg_comments:,.2f}".replace(",", "."),
+                    )
+                with col2:
+                    st.metric(
+                        label="Média de Likes",
+                        value=f"{avg_likes:,.2f}".replace(",", "."),
+                    )
+
+    def render_video_analysis(self):
+        if self.data_filtered is not None and not self.data_filtered.empty:
+            # Filtrando apenas os dados do tipo "Video"
+            video_data = self.data_filtered[self.data_filtered["type"] == "Video"]
+            video_data_frame = video_data[
+                [
+                    "videoplaycount",
+                    "videoviewcount",
+                    "likescount",
+                    "commentscount",
+                    "url",
+                    "ownerusername",
+                    "type",
+                ]
+            ]
+            if not video_data.empty:
+                st.subheader("🎥 Análise de Vídeos")
+
+                # Cálculo das médias de visualizações e reproduções de vídeos
+                video_view_mean = video_data["videoviewcount"].mean()
+                video_play_mean = video_data["videoplaycount"].mean()
+
+                # Exibição das métricas de média de visualizações e reproduções
+                col1, col2 = st.columns(2)
+                with col1:
+                    st.metric(
+                        label="Média de Visualizações de Vídeos",
+                        value=f"{video_view_mean:,.2f}".replace(",", "."),
+                    )
+                with col2:
+                    st.metric(
+                        label="Média de Reproduções de Vídeos",
+                        value=f"{video_play_mean:,.2f}".replace(",", "."),
+                    )
+
+                # Comparativo entre visualizações e reproduções de vídeos
+                fig = px.scatter(
+                    video_data,
+                    x="videoplaycount",
+                    y="videoviewcount",
+                    hover_data=["url"],
+                    custom_data=["url"],
+                    title="Relação entre Reproduções e Visualizações de Vídeos",
+                    labels={
+                        "videoplaycount": "Reproduções de Vídeos",
+                        "videoviewcount": "Visualizações de Vídeos",
+                        "url": "URL do Vídeo",
+                    },
+                    color_discrete_sequence=["#636EFA"],
+                    template="plotly_white",
+                )
+                st.plotly_chart(fig, use_container_width=True)
+
+                # Visualização do DataFrame
+                st.subheader("📊 Dados Filtrados para Análise de Vídeos")
+                st.dataframe(video_data_frame, use_container_width=True)
+
+    def render_likes_comments_scatter(self):
+        if self.data_filtered is not None and not self.data_filtered.empty:
+            st.subheader("👍 Likes x Comentários 💬")
+            filtered_data = self.data_filtered.copy()
+            filtered_data_frame = filtered_data[
+                ["likescount", "commentscount", "url", "type", "ownerusername"]
+            ]
+            filtered_data["likescount"] = filtered_data["likescount"].apply(
+                lambda x: max(0, x)
+            )
+            filtered_data["commentscount"] = filtered_data["commentscount"].apply(
+                lambda x: max(0, x)
+            )
+            fig = px.scatter(
+                filtered_data,
+                x="likescount",
+                y="commentscount",
+                title="Relacionamento entre Likes e Comentários",
+                labels={"likescount": "Likes", "commentscount": "Comentários"},
+                color_discrete_sequence=["#EF553B"],
+                template="plotly_white",
+            )
+            st.plotly_chart(fig)
+
+            # Visualização do DataFrame
+            st.subheader("📊 Dados Filtrados para Likes x Comentários")
+            st.dataframe(filtered_data_frame, use_container_width=True)
+
+    def render_observations(self):
+        if self.data_filtered is not None and not self.data_filtered.empty:
+            show_insights = st.checkbox("Ver Insights de Marketing (GPT)")
+            if show_insights:
+                st.info(
+                    "Os insights a seguir foram gerados por uma Inteligência Artificial (GPT) e devem ser utilizados como sugestões, podendo não ser 100% precisos."
+                )
+                # Integrando a análise do GPT
+                columns = self.data_filtered.columns.tolist()
+                data = self.data_filtered.values.tolist()
+                if columns and data:
+                    model = st.selectbox(
+                        "Escolha o modelo GPT:",
+                        [
+                            "gpt-4o",
+                            "gpt-4o-mini",
+                            "gpt-4-turbo",
+                            "gpt-4",
+                            "gpt-3.5-turbo",
+                        ],
+                    )
+                    temperature = st.slider(
+                        "Temperatura do Modelo:",
+                        min_value=0.0,
+                        max_value=2.0,
+                        value=0.5,
+                    )
+                    if st.button("Executar Análise de Marketing (GPT)"):
+                        insights = analyze_data_with_gpt(
+                            columns, data, model=model, temperature=temperature
+                        )
+                        if insights:
+                            st.write("## Insights de Marketing (GPT)")
+                            st.markdown(insights)
+
+    def convert_df_to_excel(self, df):
+        output = BytesIO()
+        with pd.ExcelWriter(output, engine="xlsxwriter") as writer:
+            df.to_excel(writer, index=False, sheet_name="Transações")
+        output.seek(
+            0
+        )  # Certifique-se de voltar para o início do arquivo antes de retorná-lo
+        return output.getvalue()
+
+    def render_data_table(self):
+        if self.data_filtered is not None and not self.data_filtered.empty:
+            pagina = str(self.selected_username).upper()
+
+            st.subheader(f"📋 Visualização Geral dos Dados da Página: {pagina}")
+            st.dataframe(self.data_filtered)
+
+            # Corrigido: passar o DataFrame corretamente
+            st.download_button(
+                label="📥 Baixar planilha como XLSX",
+                data=self.convert_df_to_excel(self.data_filtered),
+                file_name=f"concorrentes_({self.selected_username}).xlsx",
+                mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+            )
+
+    def render(self):
+        self.render_sidebar()
+        self.render_overview()
+        self.render_post_type_distribution()
+        self.render_general_statistics()
+        self.render_video_analysis()
+        self.render_likes_comments_scatter()
+        self.render_data_table()
+        self.render_observations()
+
+
+if __name__ == "__main__":
+    dashboard = Dashboard()
+    dashboard.render()
